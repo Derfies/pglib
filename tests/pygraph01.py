649f7651d80eebf68eba2c956400a5ee47ea03b9
Derfies <Derfies@users.noreply.github.com>
2019-09-15 21:38:05 +1000

    [EDIT] - Works, kinda.

--- tests/pygraph01.py
+++ tests/pygraph01.py
@@ -11,9 +11,9 @@ logging.basicConfig(level=logging.INFO)
 
 GRID = 40
 OFFSET = 100
-VERTEX_STROKEWIDTH = 5
+VERTEX_STROKEWIDTH = 6
 VERTEX_STROKE = (1, 0, 0, 0)
-EDGE_STROKEWIDTH = 5
+EDGE_STROKEWIDTH = 4
 EDGE_STROKE = (0, 1, 0, 0)
 WIN_WIDTH = 640
 WIN_HEIGHT = 480
@@ -25,9 +25,9 @@ class Line(object):
         offset = kwargs.pop('offset', (0, 0))
         line(
             self.x1 * GRID + OFFSET + offset[0], 
-            WIN_HEIGHT - (self.y1 * GRID + OFFSET),  
+            WIN_HEIGHT - (self.y1 * GRID + OFFSET + offset[1]),  
             self.x2 * GRID + OFFSET + offset[0],  
-            WIN_HEIGHT - (self.y2 * GRID + OFFSET),
+            WIN_HEIGHT - (self.y2 * GRID + OFFSET + offset[1]),
             **kwargs
         )
 
@@ -87,13 +87,20 @@ class Vertex(Line):
         ])
 
     def collides_with_edge(self, edge):
-        return edge.x > self.x1 and edge.x < self.x2
+        # logger.info('    self.y: {}'.format(self.y))
+        # logger.info('    edge.y1: {}'.format(edge.y1))
+        # logger.info('    edge.y2: {}'.format(edge.y2))
+        # logger.info('    self.y > edge.y1: {}'.format(self.y > edge.y1))
+        # logger.info('    self.y < edge.y2: {}'.format(self.y < edge.y2))
+        # logger.info('    edge.x > self.x1: {}'.format(edge.x > self.x1))
+        # logger.info('    edge.x < self.x2: {}'.format(edge.x < self.x2))
+        return self.y > edge.y1 and self.y < edge.y2 and edge.x > self.x1 and edge.x < self.x2
 
     def draw(self, **kwargs):
         super(Vertex, self).draw(
             stroke=VERTEX_STROKE, 
             strokewidth=VERTEX_STROKEWIDTH,
-            text_offset=(-20, 0),
+            text_offset=(-10, 0),
         )
 
     def copy(self):
@@ -107,10 +114,10 @@ class Vertex(Line):
         
 class Edge(Line):
 
-    def __init__(self, v1, v2):
+    def __init__(self, v1, v2, x):
         self.v1 = v1
         self.v2 = v2
-        self.x = self.v1.x
+        self.x = x#self.v1.x
         v1.edges.append(self)
         v2.edges.append(self)
 
@@ -120,26 +127,26 @@ class Edge(Line):
 
     @property
     def x1(self):
-        return self.x
+        return self.x#(self.v1.x1 + self.v1.x2) / 2.0
 
     @property
     def y1(self):
-        return self.v1.y1
+        return min(self.v1.y, self.v2.y)
 
     @property
     def x2(self):
-        return self.x
+        return self.x#(self.v2.x1 + self.v2.x2) / 2.0
 
     @property
     def y2(self):
-        return self.v2.y2
+        return max(self.v1.y, self.v2.y)
 
     def draw(self, **kwargs):
         super(Edge, self).draw(
             stroke=EDGE_STROKE,
             strokewidth=EDGE_STROKEWIDTH,
             offset=(GRID / 2, 0),
-            text_offset=(0, -15),
+            text_offset=(5, 20),
             font_size=8,
         ) 
 
@@ -155,15 +162,25 @@ class VisibilityGraphGenerator(object):
         self.vertices = {}
         self.edges = []
 
-    def widen_from(self, pos, amt=1):
-        logger.info('Widen from: {} @ {}'.format(pos, amt))
+
+        self.iters = 0
+
+    def get_edges(self):
+        edges = []
         for vertex in self.vertices.values():
-            if pos < vertex.x2:
-                vertex.x2 += amt
-                logger.info('Vertex: {}.x2 -> {}'.format(vertex.id, vertex.x2))
+            edges.extend(vertex.edges)
+        return edges
+
+    def widen_from(self, pos, amt=1):
+        logger.info('Widen from: {}'.format(pos))
+        for vertex in self.vertices.values():
             if pos < vertex.x1:
                 vertex.x1 += amt
                 logger.info('Vertex: {}.x1 -> {}'.format(vertex.id, vertex.x1))
+            elif pos < vertex.x2:
+                vertex.x2 += amt
+                logger.info('Vertex: {}.x2 -> {}'.format(vertex.id, vertex.x2))
+            logger.info('Vertex : {}.width -> {}'.format(vertex.id, vertex.width))
 
         for edge in self.edges:
             if pos <= edge.x:
@@ -196,8 +213,16 @@ class VisibilityGraphGenerator(object):
                 x += 1
             conn_vertex = self.vertices[conn_nidx]
 
+            # Ensure the neighbour is at least as long as to create an overlap.
+            foo = max(conn_vertex.x2, vertex.x1 + 1)
+            if foo != conn_vertex.x2:
+                conn_vertex.x2 = foo
+                logger.info('Lengthen: {}'.format(conn_vertex.x2))
+
             # Ensure the two vertices overlap.
             if not vertex.overlaps(conn_vertex):
+
+                logger.info('Want to extend vertex: {}'.format(nidx))
 
                 # We need to extend the vertex so there's an overlap point to
                 # draw and edge, but there might be an edge in the way.
@@ -206,12 +231,16 @@ class VisibilityGraphGenerator(object):
                 test_vertex.width += new_x
 
                 coll_x = None
-                for v in self.vertices.values():
-                    for edge in v.edges:
-                        if test_vertex.collides_with_edge(edge):
-                            logger.info('collide: {} {}'.format(v.id, edge.id))
-                            coll_x = edge.x
 
+                #for v in self.vertices.values():
+                #    for edge in v.edges:
+                for edge in sorted(self.get_edges(), key=lambda e: e.x):
+                        #logger.info('test: {}'.format(edge.id))
+                        if test_vertex.collides_with_edge(edge):
+                            logger.info('Collides with edge: {} @ {}'.format(edge.id, edge.x))
+                            coll_x = edge.x
+                            break
+                            #return True
                 
                 if coll_x:
                     self.widen_from(coll_x)
@@ -221,50 +250,51 @@ class VisibilityGraphGenerator(object):
 
             # Create the edge to the original vertex.
             if not conn_vertex.is_connected(vertex):
-                logger.info('Creating edge: {} -> {}'.format(nidx, conn_nidx))
-                edge = Edge(conn_vertex, vertex)
+                logger.info('Creating edge: {}|{}'.format(conn_nidx, nidx))
+                edge = Edge(conn_vertex, vertex, max(conn_vertex.x1, vertex.x1))
                 self.edges.append(edge)
-            # else:
 
-            #     conn_vertex = self.vertices[conn_nidx]
+            self.iters += 1
 
-            #     # See if we're connecting ahead.
-            #     if conn_vertex.y > vertex.y:
-            #         x2 = max(vertex.x1 + 1, conn_vertex.x2)
-            #         logger.info('Lengthen: {} -> {}'.format(conn_nidx, x2))
-            #         conn_vertex.x2 = x2
-
-            #     if vertex.x2 <= conn_vertex.x1:
-            #         logger.info('here {} {}'.format(conn_vertex.x1, vertex.x1))
-            #         #print 'here:', conn_vertex.x1, vertex.x1
-            #     #else:
-
-
-            # conn_vertex = self.vertices[conn_nidx]
-            #self.edges.append(Edge(x + 0.5, vertex.y, conn_vertex.y - vertex.y))
+            if self.iters > 4:
+                return True
 
         logger.info('')
 
     def run(self):
-        i = 0
-        #for nidx in pygraph.breadth_first_search(self.graph, root_node=1):
-        # self.process_node(1)
-        # self.process_node(2)
-        # self.process_node(2)
-            
-
+        #i = 0
         for nidx in pygraph.breadth_first_search(self.graph, root_node=1):
-            self.process_node(nidx)
-            i += 1
-            if i > 1:
-                break
-        #    print nidx, self.graph.get_node(nidx), type(self.graph.get_node(nidx))
-        #    #print dir(self.graph.get_node(nidx))
-
-
+            result = self.process_node(nidx)
+            if result:
+               break
+            #i += 1
+            #if i > 1:
+            #    break
 
 
 # Build graph.
+ln = pygraph.UndirectedGraph()
+ln.new_node()
+ln.new_node()
+ln.new_edge(1, 2)
+
+fork = pygraph.UndirectedGraph()
+fork.new_node()
+fork.new_node()
+fork.new_node()
+fork.new_node()
+fork.new_edge(1, 2)
+fork.new_edge(1, 3)
+fork.new_edge(1, 4)
+
+tri = pygraph.UndirectedGraph()
+tri.new_node()
+tri.new_node()
+tri.new_node()
+tri.new_edge(1, 2)
+tri.new_edge(2, 3)
+tri.new_edge(3, 1)
+
 square = pygraph.UndirectedGraph()
 square.new_node()
 square.new_node()
@@ -275,29 +305,58 @@ square.new_edge(2, 3)
 square.new_edge(3, 4)
 square.new_edge(4, 1)
 
-triangle = pygraph.UndirectedGraph()
-triangle.new_node()
-triangle.new_node()
-triangle.new_node()
-triangle.new_edge(1, 2)
-triangle.new_edge(2, 3)
-triangle.new_edge(3, 1)
+rect = pygraph.UndirectedGraph()
+rect.new_node()
+rect.new_node()
+rect.new_node()
+rect.new_node()
+rect.new_node()
+rect.new_node()
+rect.new_edge(1, 2)
+rect.new_edge(2, 3)
+rect.new_edge(3, 4)
+rect.new_edge(4, 5)
+rect.new_edge(5, 6)
+rect.new_edge(6, 1)
 
-graph2 = pygraph.UndirectedGraph()
-graph2.new_node()
-graph2.new_node()
-graph2.new_node()
-graph2.new_node()
-graph2.new_edge(1, 2)
-graph2.new_edge(2, 3)
-graph2.new_edge(3, 1)
+tee = pygraph.UndirectedGraph()
+tee.new_node()
+tee.new_node()
+tee.new_node()
+tee.new_node()
+tee.new_edge(1, 2)
+tee.new_edge(2, 3)
+tee.new_edge(2, 4)
 
-graph2.new_edge(4, 2)
-graph2.new_edge(4, 3)
-graph2.new_edge(4, 1)
+tee2 = pygraph.UndirectedGraph()
+tee2.new_node()
+tee2.new_node()
+tee2.new_node()
+tee2.new_node()
+tee2.new_node()
+tee2.new_node()
+tee2.new_node()
+tee2.new_edge(1, 2)
+tee2.new_edge(2, 3)
+tee2.new_edge(2, 4)
+tee2.new_edge(4, 5)
+tee2.new_edge(4, 6)
+tee2.new_edge(4, 7)
+
+tripyr = pygraph.UndirectedGraph()
+tripyr.new_node()
+tripyr.new_node()
+tripyr.new_node()
+tripyr.new_node()
+tripyr.new_edge(1, 2)
+tripyr.new_edge(2, 3)
+tripyr.new_edge(3, 1)
+tripyr.new_edge(4, 2)
+tripyr.new_edge(4, 3)
+tripyr.new_edge(4, 1)
 
 # Run the generator.
-graph_gen = VisibilityGraphGenerator(triangle)
+graph_gen = VisibilityGraphGenerator(tripyr)
 graph_gen.run()
 
 # Draw.
@@ -305,9 +364,10 @@ window = pyglet.window.Window(width=WIN_WIDTH, height=WIN_HEIGHT)
 
 @window.event
 def on_draw():
-    for vertex in graph_gen.vertices.values():
-        vertex.draw()
+    
     for edge in graph_gen.edges:
         edge.draw(font_size=8, text_offset=(-20, GRID / 2))
+    for vertex in graph_gen.vertices.values():
+        vertex.draw()
 
 pyglet.app.run()

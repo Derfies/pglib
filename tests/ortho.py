857edea1b1e2f5d7b14764651fde04ac1d94fa6d
RTRSYD\jdavies <jamie@rotorstudios.com>
2020-01-21 16:59:06 +1100

    [EDIT] - Latest.

--- tests/ortho.py
+++ tests/ortho.py
@@ -16,29 +16,47 @@ def permute_layouts(g):
     num_nodes = g.number_of_nodes()
     num_spare_nodes = num_nodes - 4
 
-    # Calculate all possible combinations of interior angles. In order for the 
+    # Calculate all possible combinations of spare angles. In order for the
     # polygon to be closed these must add up to zero.
-    all_int_angles = list(it.product([
+    spare_angle_perms = filter(lambda x: sum(x) == 0, it.product([
         INSIDE_CORNER, 
         OUTSIDE_CORNER, 
         STRAIGHT_CORNER
     ], repeat=num_spare_nodes))
-    int_angles = filter(lambda x: sum(x) != 0, all_int_angles)
 
-    angle_groups = []
-    spare_node_idx_groups = list(it.combinations(range(num_nodes), num_spare_nodes))
-    for spare_node_idxs in spare_node_idx_groups:
-        angles = tuple([
-            INSIDE_CORNER if idx not in spare_node_idxs else number_of_nodes # Do we need to do outside corner?
+    for spare_angles in spare_angle_perms:
+        print spare_angles
+
+    # Calculate all possible positions of the minimum four required inside
+    # corners.
+    inside_angle_perms = [
+        tuple([
+            INSIDE_CORNER if idx not in idxs else None # Do we need to do outside corner?
             for idx in range(num_nodes)
         ])
-        angle_groups.append(angles)
+        for idxs in it.combinations(range(num_nodes), num_spare_nodes)
+    ]
 
-        #for angles in angle_groups:
-        print angles, len(spare_node_idxs)
+    for inside_angles in inside_angle_perms:
+        print inside_angles
 
-    print len(angle_groups)
-    print len(set(angle_groups))
+    #all_angle_perms = []
+    #for
+    #
+    #angle_groups = []
+    # spare_node_idx_groups = it.combinations(range(num_nodes), num_spare_nodes)
+    # for spare_node_idxs in spare_node_idx_groups:
+    #     angles = tuple([
+    #         INSIDE_CORNER if idx not in spare_node_idxs else None # Do we need to do outside corner?
+    #         for idx in range(num_nodes)
+    #     ])
+    #     angle_groups.append(angles)
+    #
+    #     #for angles in angle_groups:
+    #     print angles, len(spare_node_idxs)
+
+    #print len(angle_groups)
+    #print len(set(angle_groups))
 
 
 g = nx.path_graph(NUM_NODES)

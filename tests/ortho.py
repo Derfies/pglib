91fd2b8f8f9c4596141dca9149ce3c4bdb18f492
Derfies <Derfies@users.noreply.github.com>
2020-01-21 21:53:27 +1100

    [EDIT] - Ortho angles calculated - now trying to calculate edge length.

--- tests/ortho.py
+++ tests/ortho.py
@@ -1,13 +1,20 @@
 import itertools as it
-import matplotlib.pyplot as plt
 
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
-NUM_NODES = 8
+NUM_NODES = 5
 INSIDE_CORNER = 90
 OUTSIDE_CORNER = -90
 STRAIGHT_CORNER = 0
+
+
+class Direction(object):
+    up = 0
+    right = 1
+    down = 2
+    left = 3
 
 
 def permute_layouts(g):
@@ -24,9 +31,6 @@ def permute_layouts(g):
         STRAIGHT_CORNER
     ], repeat=num_spare_nodes))
 
-    for spare_angles in spare_angle_perms:
-        print spare_angles
-
     # Calculate all possible positions of the minimum four required inside
     # corners.
     inside_angle_perms = [
@@ -37,8 +41,39 @@ def permute_layouts(g):
         for idxs in it.combinations(range(num_nodes), num_spare_nodes)
     ]
 
-    for inside_angles in inside_angle_perms:
-        print inside_angles
+    # Create all combinations using the above.
+    angle_perms = []
+    for spare_angles in spare_angle_perms:
+        for inside_angles in inside_angle_perms:
+            spare_angles_iter = iter(spare_angles)
+            angle_perms.append(tuple([
+                inside_angle or spare_angles_iter.next()
+                for inside_angle in inside_angles
+            ]))
+
+    print 'total:', len(angle_perms)
+    print 'unique:', len(set(angle_perms))
+
+    # for foo in sorted(set(angle_perms)):
+    #     print foo
+
+    # Walk polygon edge clockwise.
+    
+    direction = Direction.up
+    for angles in angle_perms:#[0:1]:
+        edges = {}
+        for i, angle in enumerate(angles):
+            edges.setdefault(direction, []).append(i)
+            if angle == INSIDE_CORNER:
+                direction += 1
+            elif angle == OUTSIDE_CORNER:
+                direction -= 1
+            direction = direction % 4
+
+        print 'angles:', angles
+        print 'edges:', edges
+
+    # Walk polygon edges and put into buckets 
 
     #all_angle_perms = []
     #for

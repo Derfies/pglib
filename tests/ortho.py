d66cc7e23381af30f44a990c85b5b8c2361c9709
Derfies <Derfies@users.noreply.github.com>
2020-01-24 11:17:22 +1100

    [EDIT] - More cleanup and refactor.

--- tests/ortho.py
+++ tests/ortho.py
@@ -5,44 +5,63 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-NUM_NODES = 6
-INSIDE_CORNER = 90
-OUTSIDE_CORNER = -90
-STRAIGHT_CORNER = 0
+NUM_NODES = 5
 LENGTH = 'length'
 
 
 class Direction(enum.IntEnum):
+
     up = 0
     right = 1
     down = 2
     left = 3
 
     @staticmethod
+    def normalise(direction):
+        return Direction(direction % 4)
+
+    @staticmethod
     def opposite(direction):
-        return Direction((direction - 2) % 4)
+        return Direction.normalise(direction - 2)
+
+    @staticmethod
+    def xs():
+        return (Direction.left, Direction.right)
+
+    @staticmethod
+    def ys():
+        return (Direction.up, Direction.down)
 
 
-X_DIRS = (Direction.left, Direction.right)
-Y_DIRS = (Direction.up, Direction.down)
+class Angle(enum.IntEnum):
+
+    inside = 90
+    outside = -90
+    straight = 0
 
 
+class Base(object):
 
-class Layout(object):
+    def edge_walk(self, direction):
+        for idx, edge in enumerate(self.edges):
+            yield edge, direction
+            if self.angles[idx] == Angle.inside:
+                direction += 1
+            elif self.angles[idx] == Angle.outside:
+                direction -= 1
+            direction = Direction.normalise(direction)
+
+
+class Layout(Base):
 
     def __init__(self, g, angles):
+        super(Base, self).__init__()
+
         self.g = g
         nx.set_edge_attributes(self.g, 1, LENGTH)
         self.angles = angles
         self.edges = self._get_face_edges()
         self.edge_directions = self._get_edge_directions()
-
-        
-        print self.width, self.height
-
-
-        
-
 
     def get_direction_length(self, direction):
         return sum([
@@ -52,11 +71,11 @@ class Layout(object):
     
     @property
     def width(self):
-        return max(map(self.get_direction_length, X_DIRS))
+        return max(map(self.get_direction_length, Direction.xs()))
 
     @property
     def height(self):
-        return max(map(self.get_direction_length, Y_DIRS))
+        return max(map(self.get_direction_length, Direction.ys()))
 
     def _get_face_edges(self):
         """
@@ -71,103 +90,40 @@ class Layout(object):
         return edges
 
     def _get_edge_directions(self):
-
-        edge_directions = {}
-        print '\nangles:', self.angles
-        direction = Direction.up
-        for idx, edge in enumerate(self.edges):
-            print '    ', idx, edge, direction
-            edge_directions.setdefault(direction, []).append(edge)
-            if self.angles[idx] == INSIDE_CORNER:
-                direction += 1
-            elif self.angles[idx] == OUTSIDE_CORNER:
-                direction -= 1
-            direction = Direction(int(direction) % 4)
-
-        print 'edges:'#, edges
-        for direction, idx in edge_directions.items():
-            print '    ', direction, idx
-
-        return edge_directions
-
-        # Calculate max bounds.
-        #self.x = max([len(idxs) for direction, idxs in edges.items() if direction in X_DIRS])
-        #self.y = max([len(idxs) for direction, idxs in edges.items() if direction in Y_DIRS])
-
-        #print 'width:', self.width
-        #print 'height:', self.height
+        directions = {}
+        for edge, direction in list(self.edge_walk(Direction.up)):
+            directions.setdefault(direction, []).append(edge)
+        return directions
 
     def permute_polygons(self):
 
         polygons = []
 
-
         foobar = {}
-
-        for axis in (X_DIRS, Y_DIRS):
+        for axis in (Direction.xs(), Direction.ys()):
             lengths = {d: self.get_direction_length(d) for d in axis}
             min_dir = min(lengths, key=lengths.get)
             min_length, max_length = lengths[min_dir], lengths[Direction.opposite(min_dir)]
-            if True:#min_length != max_length:
-                #print 'permute:', min_dir, min_length
-                print 'min_dir:', min_dir
-                # print 'opposite:', Direction.opposite(min_dir)
-                # print 'min_length:', min_length
-                # print 'max_length:', max_length
-                # print 'permute:', min_dir
-                # print 'num nodes available:', len(self.edge_directions[min_dir])
-                # print 'required length:', max_length
-                # print 'range:', range(1, max_length)
-                # print 'product:', list(it.product(range(1, max_length + 1), repeat=min_length))
-
-                #print list(it.product(range(1, max_length), repeat=len(self.edge_directions[min_dir])))
-                all_length_perms = it.product(range(1, max_length + 1), repeat=min_length)
-                length_perms = filter(lambda x: sum(x) == max_length, all_length_perms)
-
-
-                foobar[min_dir] = length_perms#, []).append(length_perms)
-
-                # for lengths in length_perms:
-                #     print 'lengths:', lengths
-                #     length_iter = iter(lengths)
-                #     poly = Polygon(self.g.copy(), self.angles, self.edges)
-                #     # #g = self.g.copy()
-                #     for edge in self.edge_directions[min_dir]:
-                #         poly.g[edge[0]][edge[1]][LENGTH] = length_iter.next()
-
-                #     print poly.g.edges(data=True)
-                #     print poly.vertex_positions()
-
-                #     polygons.append(poly)
+            all_length_perms = it.product(range(1, max_length + 1), repeat=min_length)
+            length_perms = filter(lambda x: sum(x) == max_length, all_length_perms)
+            foobar[min_dir] = length_perms
                       
-
-        print 'foobar:', foobar
         for perm in [dict(zip(foobar, v)) for v in it.product(*foobar.values())]:
-            #print '->', bar
-
             poly = Polygon(self.g.copy(), self.angles, self.edges)
-
             for dir_, lengths in perm.items():
-
-                # for lengths in length_perms:
-                #print 'lengths:', lengths
                 length_iter = iter(lengths)
-                
-                # #g = self.g.copy()
                 for edge in self.edge_directions[dir_]:
                     poly.g[edge[0]][edge[1]][LENGTH] = length_iter.next()
-
-                #print poly.g.edges(data=True)
-                #print poly.vertex_positions()
-
             polygons.append(poly)
 
         return polygons
 
 
-class Polygon(object):
+class Polygon(Base):
 
     def __init__(self, g, angles, edges):
+        super(Base, self).__init__()
+
         self.g = g
         self.angles = angles
         self.edges = edges
@@ -175,16 +131,10 @@ class Polygon(object):
     def vertex_positions(self):
         positions = {}
         pos = [0, 0]
-
-        direction = Direction.up
-        for idx, edge in enumerate(self.edges):
-            #print '    ', idx, edge, direction
-            #edge_directions.setdefault(direction, []).append(edge)
-            
+        for edge, direction in list(self.edge_walk(Direction.up)):
+            idx = edge[0]
             length = self.g[edge[0]][edge[1]][LENGTH]
             positions[idx] = pos[:]
-
-            #next_pos = pos[:]
             if direction == Direction.up:
                 pos[1] += length
             elif direction == Direction.right:
@@ -193,16 +143,6 @@ class Polygon(object):
                 pos[1] -= length
             else:   # Direction.left
                 pos[0] -= length
-
-            
-
-            if self.angles[idx] == INSIDE_CORNER:
-                direction += 1
-            elif self.angles[idx] == OUTSIDE_CORNER:
-                direction -= 1
-            direction = Direction(int(direction) % 4)
-
-
         return positions
 
 
@@ -215,17 +155,14 @@ def permute_layouts(g):
     # Calculate all possible combinations of spare angles. Filter out all
     # combinations that do not add up to zero, as these will leave the polygon
     # unclosed.
-    spare_angle_perms = filter(lambda x: sum(x) == 0, it.product([
-        INSIDE_CORNER, 
-        OUTSIDE_CORNER, 
-        STRAIGHT_CORNER
-    ], repeat=num_spare_nodes))
+    all_spare_angle_perms = it.product(Angle, repeat=num_spare_nodes)
+    spare_angle_perms = filter(lambda x: sum(x) == 0, all_spare_angle_perms)
 
     # Calculate all possible positions of the minimum four required inside
     # corners.
     inside_angle_perms = [
         tuple([
-            INSIDE_CORNER if idx not in idxs else None # Do we need to do outside corner?
+            Angle.inside if idx not in idxs else None # Do we need to do outside corner?
             for idx in range(num_nodes)
         ])
         for idxs in it.combinations(range(num_nodes), num_spare_nodes)
@@ -243,16 +180,29 @@ def permute_layouts(g):
 
     print 'total:', len(angle_perms)
     print 'unique:', len(set(angle_perms))
+    return [Layout(g.copy(), angles) for angles in set(angle_perms)]
 
-    # Walk polygon edges clockwise and put into buckets.
-    polys = []
-    for angles in set(angle_perms):
-        layout = Layout(g.copy(), angles)
-        polys.extend(layout.permute_polygons())
 
-    return polys
-        #for polygon in polygons:
-        #    print 'polygon:', polygon.g.edges(data=True)
+def init_pyplot(figsize):
+
+    # Set pyplot dimensions.
+    plt.figure(figsize=figsize)
+
+    # Then we set up our axes (the plot region, or the area in which we plot things).
+    # Usually there is a thin border drawn around the axes, but we turn it off with `frameon=False`.
+    ax = plt.axes([0,0,1,1], frameon=False)
+
+    # Then we disable our xaxis and yaxis completely. If we just say 
+    # plt.axis('off'), they are still used in the computation of the image 
+    # padding.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Even though our axes (plot region) are set to cover the whole image with 
+    # [0,0,1,1], by default they leave padding between the plotted data and the 
+    # frame. We use tigher=True to make sure the data gets scaled to the full 
+    # extents of the axes.
+    plt.autoscale(tight=True)
 
 
 # Outright fail if there are less than 4 nodes. We can change this to try to
@@ -264,42 +214,28 @@ assert g.number_of_nodes() >= 4, 'Cannot close polygon with less than 4 nodes'
 nodes = list(g.nodes())
 g.add_edge(nodes[-1], nodes[0])
 
-plt.figure(figsize=(25, 3))
-#plt.margins(x=0, y=0)
 
-# Then we set up our axes (the plot region, or the area in which we plot things).
-# Usually there is a thin border drawn around the axes, but we turn it off with `frameon=False`.
-ax = plt.axes([0,0,1,1], frameon=False)
 
-# Then we disable our xaxis and yaxis completely. If we just say plt.axis('off'),
-# they are still used in the computation of the image padding.
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+g = nx.relabel_nodes(g, {
+    n: chr(97 + n) for n in range(len(g.nodes()))
+})
 
-# Even though our axes (plot region) are set to cover the whole image with [0,0,1,1],
-# by default they leave padding between the plotted data and the frame. We use tigher=True
-# to make sure the data gets scaled to the full extents of the axes.
-plt.autoscale(tight=True)
+init_pyplot((25, 3))
+
+
+layouts = permute_layouts(g)
+polys = []
+for layout in layouts:
+    polys.extend(layout.permute_polygons())
 
 buff = 1
 margin = 0
-polys = permute_layouts(g)
 for i, poly in enumerate(polys):
     poss = poly.vertex_positions()
     old_poss = poss.copy()
-    
     for nidx, pos in poss.items():
-        #print pos
-        pos[0] += margin#(BUFFER * i)
-
-    print '->', i, margin, poss
-
+        pos[0] += margin
     margin = max([p[0] for p in old_poss.values()]) + 1 
-    print 'margin now:', margin
-        #pos[0] = pos[0] / len(polys)
-    #for p in pos:
-    #    print p
-        #pos[0] += i
     nx.draw_networkx(poly.g, pos=poss)
 
 

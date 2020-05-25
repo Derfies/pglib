1b5dd0ba5b11e2e84bb165bbcd2ffbf7ffe7f8e8
Derfies <Derfies@users.noreply.github.com>
2020-05-25 18:47:55 +1000

    [EDIT] - New point and matrix classes.

---
+++ pglib/geometry/matrix.py
@@ -0,0 +1,81 @@
+import math
+
+import numpy as np
+
+from transformations import (
+    concatenate_matrices,
+    translation_matrix,
+    rotation_matrix,
+)
+
+
+class Matrix4(object):
+
+    def __init__(self, array=None):
+        self.array = array if array is not None else np.identity(4, dtype=np.float64)
+
+    @classmethod
+    def translation_matrix(cls, t):
+        m = cls()
+        m.translate(t)
+        return m
+
+    @classmethod
+    def rotation_matrix(cls, angle, axis):
+        m = cls()
+        m.rotate(angle, axis)
+        return m
+
+    @classmethod
+    def scale_matrix(cls, s):
+        m = cls()
+        m.scale(s)
+        return m
+
+    def __str__(self):
+        return str(self.array)
+
+    def __copy__(self):
+        return self.__class__(self.array.copy())
+
+    def __mul__(self, other):
+        return self.__class__(concatenate_matrices(self.array, other.array))
+
+    def translate(self, t):
+        ta = translation_matrix((t[0], t[1], t[2]))
+        self.array = concatenate_matrices(self.array, ta)
+
+    def translate_x(self, x):
+        self.translate((x, 0, 0))
+
+    def translate_y(self, y):
+        self.translate((0, y, 0))
+
+    def translate_z(self, z):
+        self.translate((0, 0, z))
+
+    def rotate(self, angle, axis, origin=None):
+        ra = rotation_matrix(math.radians(angle), axis, origin)
+        self.array = concatenate_matrices(self.array, ra)
+
+    def rotate_x(self, x):
+        self.rotate(x, (1, 0, 0))
+
+    def rotate_y(self, y):
+        self.rotate(y, (0, 1, 0))
+
+    def rotate_z(self, z):
+        self.rotate(z, (0, 0, 1))
+
+    def scale(self, s):
+        sa = np.diag([s[0], s[1], s[2], 1.0])
+        self.array = concatenate_matrices(self.array, sa)
+
+    def scale_x(self, x):
+        self.scale((x, 1, 1))
+
+    def scale_y(self, y):
+        self.scale((1, y, 1))
+
+    def scale_z(self, z):
+        self.scale((1, 1, z))
--- pglib/geometry/point.py
+++ pglib/geometry/point.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Point2d(object):
 
     def __init__(self, x, y):
@@ -36,3 +39,81 @@ class Point2d(object):
     @y.setter
     def y(self, value):
         self[1] = value
+
+
+class Point4(object):
+
+    """
+    TODO: Differentiate point and vector.
+    "not all points are vectors, but all vectors are points"
+
+    https://stackoverflow.com/questions/3913832/what-is-a-3d-vector-and-how-does-it-differ-from-a-3d-point
+
+    """
+
+    def __init__(self, x=0, y=0, z=0, w=0):
+        self.array = np.array([x, y, z, w], dtype=np.float64)
+
+    def __str__(self):
+        return str(self.array)
+
+    def __copy__(self):
+        return self.__class__(self.array.copy())
+
+    def __add__(self, other):
+        if isinstance(other, Point4):
+            other = other.array
+        return self.__class__(*self.array + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Point4):
+            other = other.array
+        return self.__class__(*self.array - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Point4):
+            other = other.array
+        return self.__class__(*self.array * other)
+
+    def __div__(self, other):
+        if isinstance(other, Point4):
+            other = other.array
+        return self.__class__(*self.array / other)
+
+    def __getitem__(self, index):
+        return self.array[index]
+
+    def __setitem__(self, index, value):
+        self.array[index] = value
+
+    @property
+    def x(self):
+        return self.array[0]
+
+    @x.setter
+    def x(self, x):
+        self.array[0] = x
+
+    @property
+    def y(self):
+        return self.array[1]
+
+    @y.setter
+    def y(self, y):
+        self.array[1] = y
+
+    @property
+    def z(self):
+        return self.array[2]
+
+    @z.setter
+    def z(self, z):
+        self.array[2] = z
+
+    @property
+    def w(self):
+        return self.array[3]
+
+    @w.setter
+    def w(self, w):
+        self.array[3] = w

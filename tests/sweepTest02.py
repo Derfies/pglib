6e766a95ced444bd6f63799d80bcb1dd1805cd67
Derfies <Derfies@users.noreply.github.com>
2020-06-18 21:25:24 +1000

    [EDIT] - Seems to be resolving graph correctly.

--- tests/sweepTest02.py
+++ tests/sweepTest02.py
@@ -34,66 +34,90 @@ class Event(object):
 
 rs = []
 
+# One
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+
+# Two x align
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(0, 20), Vector2(10, 30)))
+
+# Two x align overlap
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(0, 5), Vector2(10, 15)))
+
+# Two y align
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(20, 0), Vector2(30, 10)))
+
+# Two y align overlap
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(5, 0), Vector2(15, 10)))
+
+# Two corner touching
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(10, 10), Vector2(20, 20)))
+
+# Two corner overlap
 # rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
 # rs.append(Rect(Vector2(5, 5), Vector2(15, 15)))
+
+# 2 x 2 grid
+# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+# rs.append(Rect(Vector2(0, 10), Vector2(10, 20)))
+# rs.append(Rect(Vector2(10, 10), Vector2(20, 20)))
+# rs.append(Rect(Vector2(10, 0), Vector2(20, 10)))
+
+
+rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+rs.append(Rect(Vector2(5, 5), Vector2(15, 15)))
 rs.append(Rect(Vector2(15, 8), Vector2(20, 18)))
-# rs.append(Rect(Vector2(15, 5), Vector2(25, 15)))
-#
-#
-# rs.append(Rect(Vector2(5, 5), Vector2(25, 15)))
+rs.append(Rect(Vector2(25, 8), Vector2(30, 18)))
+rs.append(Rect(Vector2(14, 5), Vector2(25, 15)))
+
+rs.append(Rect(Vector2(5, 5), Vector2(25, 15)))
+
 rs.append(Rect(Vector2(5, 8), Vector2(25, 18)))
-# rs.append(Rect(Vector2(20, 5), Vector2(40, 15)))
-# rs.append(Rect(Vector2(20, 10), Vector2(40, 20)))
-# rs.append(Rect(Vector2(35, 5), Vector2(55, 15)))
-#
-#
-# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
-# rs.append(Rect(Vector2(8, 8), Vector2(18, 18)))
-# rs.append(Rect(Vector2(16, 0), Vector2(26, 10)))
-#
-# rs.append(Rect(Vector2(10, 28), Vector2(20, 38)))
-# rs.append(Rect(Vector2(10, 30), Vector2(20, 40)))
-# rs.append(Rect(Vector2(10, 32), Vector2(20, 42)))
-# rs.append(Rect(Vector2(0, 35), Vector2(10, 45)))
-#
-#
-# rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
-#
-# rs.append(Rect(Vector2(5, 5), Vector2(15, 15)))
-#
-# rs.append(Rect(Vector2(15, 35), Vector2(25, 45)))
-#
-# rs.append(Rect(Vector2(60, 0), Vector2(70, 70)))
-# rs.append(Rect(Vector2(65, 20), Vector2(75, 30)))
-#
-#
-# rs.append(Rect(Vector2(0, 50), Vector2(10, 60)))
-# rs.append(Rect(Vector2(5, 50), Vector2(15, 60)))
 
 
-def get_events(rects):
-    events = {}
-    for rect in rects:
-        events.setdefault(rect.p1.x, []).append(Event(EventType.BEGIN, rect))
-        events.setdefault(rect.p2.x, []).append(Event(EventType.END, rect))
-    return events
+rs.append(Rect(Vector2(20, 5), Vector2(40, 15)))
+rs.append(Rect(Vector2(20, 10), Vector2(40, 20)))
+rs.append(Rect(Vector2(35, 5), Vector2(55, 15)))
 
 
-def get_extents(intervals):
-    extents = set()
-    for interval in intervals:
-        extents.add(interval.begin)
-        extents.add(interval.end)
-    return extents
+rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+rs.append(Rect(Vector2(8, 8), Vector2(18, 18)))
+rs.append(Rect(Vector2(16, 0), Vector2(26, 10)))
 
+rs.append(Rect(Vector2(10, 28), Vector2(20, 38)))
+rs.append(Rect(Vector2(10, 30), Vector2(20, 40)))
+rs.append(Rect(Vector2(10, 32), Vector2(20, 42)))
+rs.append(Rect(Vector2(0, 35), Vector2(10, 45)))
+
+
+rs.append(Rect(Vector2(0, 0), Vector2(10, 10)))
+
+rs.append(Rect(Vector2(5, 5), Vector2(15, 15)))
+
+rs.append(Rect(Vector2(15, 35), Vector2(25, 45)))
+
+rs.append(Rect(Vector2(60, 0), Vector2(70, 70)))
+rs.append(Rect(Vector2(65, 20), Vector2(75, 30)))
+
+
+rs.append(Rect(Vector2(0, 50), Vector2(10, 60)))
+rs.append(Rect(Vector2(5, 50), Vector2(15, 60)))
+
+
+
+
+events = {}
+for rect in rs:
+    events.setdefault(rect.p1.x, []).append(Event(EventType.BEGIN, rect))
+    events.setdefault(rect.p2.x, []).append(Event(EventType.END, rect))
 
 g = nx.Graph()
-
-
 verts = []
-
 tree = IntervalTree()
-events = get_events(rs)
 open_verts = {}
 for t in sorted(events):
 
@@ -114,42 +138,45 @@ for t in sorted(events):
             t_tree.slice(overlap.begin)
             t_tree.slice(overlap.end)
 
-    # At this point all the vertical edges that lie on T are represented as
-    # intervals which do not overlap.
-
-    begins = get_extents(intervals[EventType.BEGIN])
-    ends = get_extents(intervals[EventType.END])
-    all_ = get_extents(t_tree)
-
-    begin_and_ends = begins.intersection(ends)
-    begins -= begin_and_ends
-    ends -= begin_and_ends
-    all_ |= begin_and_ends
-
-    for vert in begins:
-        print 'start:', vert
-        open_verts[vert] = t
-
-    for vert in ends:
-        print 'end:', vert
-        try:
-            g.add_edge((open_verts[vert], vert), (t, vert))
-            del open_verts[vert]
-        except:
-            print 'failed to add edge'
-
-
-    for vert in all_ - begins - ends:
-        print 'split:', vert
-        g.add_edge((open_verts[vert], vert), (t, vert))
-        open_verts[vert] = t
-
+    # Every interval that was either started or sliced becomes an open vert that
+    # expects an edge.
     for i in t_tree:
         g.add_edge((t, i.begin), (t, i.end))
+        print 'vertical edge:', (t, i.begin), (t, i.end)
         verts.extend((Vector2(t, i.begin), Vector2(t, i.end)))
 
-    tree.update(IntervalTree(intervals[EventType.BEGIN]))
-    tree.difference_update(IntervalTree(intervals[EventType.END]))
+        # Connect to last one here
+        # Consider collecting verts and putting this in a new loop to remove
+        # duplicates
+        if i.begin in open_verts:
+            g.add_edge((open_verts[i.begin], i.begin), (t, i.begin))
+            print 'horizontal edge (begin):', (open_verts[i.begin], i.begin), (t, i.begin)
+        if i.end in open_verts:
+            g.add_edge((open_verts[i.end], i.end), (t, i.end))
+            print 'horizontal edge (end):', (open_verts[i.end], i.end), (t, i.end)
+
+    for i in t_tree:
+        print 'open vert: ({}, {})'.format(t, i.begin,)
+        open_verts[i.begin] = t
+        print 'open vert: ({}, {})'.format(t, i.end)
+        open_verts[i.end] = t
+
+    tree.update(intervals[EventType.BEGIN])
+
+    # Build max x extend dict.
+    rects = [i.data for i in tree]
+    max_edges = {}
+    for rect in rects:
+        max_edges[rect.p1.y] = max(max_edges.get(rect.p1.y), rect.p2.x)
+        max_edges[rect.p2.y] = max(max_edges.get(rect.p2.y), rect.p2.x)
+
+    for y in open_verts.keys():
+        if y in max_edges and t == max_edges[y]:
+            print 'close vert: ({}, {}) because t == max_edges[y]'.format(max_edges[y], y)
+            del open_verts[y]
+
+
+    tree.difference_update(intervals[EventType.END])
 
 
 pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
@@ -189,8 +216,13 @@ def on_draw():
 
 pyglet.app.run()
 #pos = nx.nx_agraph.graphviz_layout(g, prog='neato')
-pos = nx.spring_layout(g)
-nx.draw(g, pos=pos)
-nx.draw_networkx_labels(g, pos=pos)
+#print pos
+pos = {
+    node: node for node in g
+}
+
+#pos = nx.spring_layout(g)
+nx.draw(g, pos=pos, width=8, alpha=0.5, edge_color='b')
+nx.draw_networkx_labels(g, pos=pos, font_size=8)
 plt.show()
 
